470238eb0761fd36aa5e16d8dc770f52eae393b8
Jeremia Manogi <jeremiamanogi@Jeremias-Laptop.local>
2025-08-14 16:35:01 +0700

    adding FLAG_IMMUTABLE

--- sample/src/main/java/com/yalantis/ucrop/sample/ResultActivity.java
+++ sample/src/main/java/com/yalantis/ucrop/sample/ResultActivity.java
@@ -211,7 +211,7 @@ public class ResultActivity extends BaseActivity {
                 .setTicker(getString(R.string.notification_image_saved))
                 .setSmallIcon(R.drawable.ic_done)
                 .setOngoing(false)
-                .setContentIntent(PendingIntent.getActivity(this, 0, intent, 0))
+                .setContentIntent(PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE))
                 .setAutoCancel(true);
         if (notificationManager != null) {
             notificationManager.notify(DOWNLOAD_NOTIFICATION_ID_DONE, notificationBuilder.build());
